998a981d7833c387d9ab82d39a81618c540c2e08
Matt Bow <mattybow@gmail.com>
2013-07-11 02:44:32 +0000

    redirected to index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var fsOut = fs.readFileSync(index.html);
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(fsOut);
 });
 
 var port = process.env.PORT || 5000;
