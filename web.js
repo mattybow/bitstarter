14a0096925ebba42155d47a03b3f0cf4871e8baf
Matt Bow <mattybow@gmail.com>
2013-07-11 03:01:05 +0000

    fixed bugs with reading buffer

--- web.js
+++ web.js
@@ -3,11 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
+ 
+var fsOut = fs.readFileSync('index.html');
 
-var fsOut = fs.readFileSync(index.html);
+var printFsOut = fsOut.toString('utf8');
 
 app.get('/', function(request, response) {
-  response.send(fsOut);
+  response.send(printFsOut);
 });
 
 var port = process.env.PORT || 5000;
