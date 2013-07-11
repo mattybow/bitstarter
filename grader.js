5eca4bdee791546a22b668624178a4b85ae41e12
Matt Bow <mattybow@gmail.com>
2013-07-11 05:57:29 +0000

    revised code to take in web address

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler')
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -37,11 +38,15 @@ var assertFileExists = function(infile) {
 };
 
 var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+    return cheerio.load(htmlfile);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var htmlReq = function(url){
+    return rest.get(url)
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
@@ -64,9 +69,13 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-u, --url <url_path>', 'Path to checks.json', "http://google.com")
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var httpReq = rest.get(program.url);
+    //console.log(httpReq);
+    var checkJson = checkHtmlFile(httpReq, program.checks);
+    //console.log("ok");
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
